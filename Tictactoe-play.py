de608d4e2e44934db39660a81522f9dd3a33f991
Nat-Reid <nathanielreid@gmail.com>
2019-06-10 13:35:50 -0700

    rewrote some comments

--- Tictactoe-play.py
+++ Tictactoe-play.py
@@ -9,7 +9,7 @@ VERBOSE = 0
 print "asd"
 import numpy as np
 #np.random.seed(1)
-length = 9 #dont't touch, length nine is necessary for tic tac toe
+length = 9 #dont't touch, length nine is necessary for tic tac toe board
 hidlaysiz = 16
 trainsize = 1
 board = np.array([[0,1,1],[1,0,0],[0,1,1]])
@@ -22,8 +22,6 @@ syn2 = 2*np.random.random((hidlaysiz,1)) - 1
 syn1 = 2*np.random.random((hidlaysiz,hidlaysiz)) - 1
 syn0 = 2*np.random.random((length,hidlaysiz)) - 1
 #the important probability curve that you took forever to understand
-#you still don't fully understand why it works
-#what the fuck
 def sigmoid(x,deriv=False):
     if (deriv==True):
         return x*(1-x)
@@ -37,30 +35,31 @@ def train_generation(X,y,syn0,syn1,syn2,it,it_):
     l1 = sigmoid(np.dot(l0,syn0))
     l2 = sigmoid(np.dot(l1,syn1))
     l3 = sigmoid(np.dot(l2,syn2))
-    
+
     #how far was NN guess from correct answer, now with two layers
     l3_error = y - l3
     if (it%100 == 0):
         if (it_%1000 == 0):
+            print "sample error"
             print np.mean(np.abs(l3_error))
-            
+
     #delta is error multiplied by deriv of sigmoid
     #deriv of sigmoid is "how sure" weights are about answer, so if it is more
     #sure, the delta is less and the weight changes less
     l3_delta = l3_error * sigmoid(l3,True)
-    
+
     #this calculation of l1 and l2 error is back propagation
     l2_error = l3_delta.dot(syn2.T)
     l2_delta = l2_error * sigmoid(l2,True)
     l1_error = l2_delta.dot(syn1.T)
     l1_delta = l1_error * sigmoid(l1,True)
-    
+
     #adjusting weights by delta
-    syn2 += l2.T.dot(l3_delta) 
+    syn2 += l2.T.dot(l3_delta)
     syn1 += l1.T.dot(l2_delta)
     syn0 += l0.T.dot(l1_delta)
-    
-    return syn0,syn1,syn2 
+
+    return syn0,syn1,syn2
 
 def propogate(l0,syn0,syn1,syn2):
     #l0 = [[0,1,1,1,0,0,0,1,1]]
@@ -81,7 +80,7 @@ def isgameover (board):
     if (board[2] == board[4] == board[6])and (board[2] != 0):
         num = board[2]
     return num
-    
+
 def create_data():
     print '1'
     dnumber = 0
@@ -111,12 +110,14 @@ def create_data():
                 game_point[moves[x]] = 1
             if (x%2 == 1):
                 game_point[moves[x]] = 2
+        print "game position in 3*3"
         print game_point.reshape(3,3)
+        print "game position not reshaped"
         print game_point
         for x in range (5):
             new_moves = np.random.permutation(moves[show_point:])
             random_outcome = 1* game_point
-            if (VERBOSE > 1): 
+            if (VERBOSE > 1):
                 print new_moves, random_outcome
             move = 0
             end = 0
@@ -129,26 +130,25 @@ def create_data():
                 move += 1
                 end = isgameover(random_outcome)
                 print end
-            print random_outcome.reshape(3,3) 
-            
+            print random_outcome.reshape(3,3)
+
         if (end == 0):
             result_set [dnumber] = 0.5
         if (end == 1):
             result_set [dnumber] = 1
         if (end == 2):
             result_set [dnumber] = 0
-                       
+
         dnumber +=1
 
     #return test_set,result_set
-     
+
 """for x in range(400):
     inputs,results = create_data()
     for y in range(4000):
         syn0,syn1,syn2 = train_generation(inputs,results,syn0,syn1,syn2,x,y)"""
 create_data()
-
-'''for x in range (601):     
+for x in range (601):
     num = 0
     y = []
     input_data = np.zeros((trainsize,length))
@@ -176,13 +176,12 @@ create_data()
     y = y.reshape((trainsize,1))
     for z in range(1000):
         syn0,syn1,syn2 = train_generation(input_data,y,syn0,syn1,syn2,x,z)
-            
-'''
+
 '''print round(l3)
 print l3
 print syn0
 print ''
 print syn1
-print '' 
+print ''
 print syn2
 '''
